7f275c36fe523a062df2ceaa1e85852c4319177a
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-11-04 17:29:16 +0100

    Cambios para la muestra en netlify

--- src/main/java/org/example/models/Calculadora.java
+++ src/main/java/org/example/models/Calculadora.java
@@ -1,18 +1,45 @@
 package org.example.models;
 
+/**
+ * Clase calculadora con métodos de calculo básicos
+ */
 public class Calculadora {
+    /**
+     * Función que suma dos números enteros pasados por parámetros
+     * @param a número entero
+     * @param b número entero
+     * @return la suma de a y b
+     */
     public int sumar(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Función que resta dos números enteros pasados por parámetros
+     * @param a número entero
+     * @param b número entero
+     * @return la resta de a y b
+     */
     public int restar(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Función que multiplica dos números enteros
+     * @param a número entero
+     * @param b número entero
+     * @return la multiplicación de a por b
+     */
     public int multiplicar(int a, int b) {
         return a * b;
     }
 
+    /**
+     * Función que divide dos números enteros
+     * @param a número entero
+     * @param b número entero
+     * @return la división de a entre b
+     **/
     public double dividir(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("No se puede dividir entre cero");
